65c4e6303495ce1dc8c8248daa78196daeb4bbd2
Iltabish Khan <ladly5008@gmail.com>
2023-11-19 09:58:43 +0530

    Fix line endings in package.json

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const  express = require('express')
 const path=require('path');
-
+require("./db/conn");
+const Register=require("./models/register");
 const app = express()
 const port = 3004
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -21,6 +25,18 @@ app.get('/about',(req,res)=>{
 app.get('/contact',(req,res)=>{
         res.sendFile(path.join(__dirname,'/templates/contact.html'))
          })
+
+         app.post("/contact",async(req,res)=>{
+          try {
+              const registerStudent=new Register(req.body);
+             const registered=await registerStudent.save();
+             res.status(201).sendFile(path.join(__dirname,'/templates/hello.html'));
+          } catch (error) {
+            console.error(error);
+              res.status(400).send("OOPserror");
+          }
+      });
+            
         
 app.listen(port,  () => {
             console.log(`Blog app listening at http://localhost:${port}`);
